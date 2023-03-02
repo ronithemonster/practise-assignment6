d1b04fb715c77952b4baa5a9f89e1717ad60ce71
ronithemonster <ashadul15-3182@diu.edu.bd>
2023-03-02 23:11:32 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,37 @@
-const loadPhone = async() =>{
-    const url =`https://openapi.programming-hero.com/api/ai/tools`;
-    const res = await fetch(url)
-    const data = await res.json()
-    displayAll(data.data);
+const loadPhone = () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAll(data.data.tools));
 }
 
-const displayAll= (universes) =>{
+const displayAll = (universes) => {
+    const ContainerDiv = document.getElementById('card-container');
     universes.forEach(universe => {
-        console.log(universe);
+          console.log(universe);
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="col">
+                  <div class="card h-100">
+                    <img src="${universe.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h2 class="card-title">Features</h2>
+                      <p class="card-text"><ul>
+                      <li>1.Natural language processing</li>
+                      <li>2.Contextual understanding</li>
+                      <li>3.Text generation</li>
+                  </ul>
+                  </p>
+                    </div>
+                    <div class="card-footer">
+                      <h2 class="">${universe.name}</h2>
+                      <p>${universe.published_in}</p>
+                    </div>
+                  </div>
+                </div>
+        `
+        ContainerDiv.appendChild(cardDiv);
     });
 }
 loadPhone();
